6773de5724df0552697e6d78c19d88f634f230fe
Meret Unbehaun <meret.unbehaun@outlook.com>
2023-05-18 12:02:49 +0200

    Add interpretation as generator

--- Definitions/FreeGen.hs
+++ Definitions/FreeGen.hs
@@ -1,5 +1,7 @@
 module FreeGen where
 
+import Test.QuickCheck (Gen)
+import qualified Test.QuickCheck.Gen as Gen
 
 -- Basic definition of the type FreeGen (and other necessary definitions)
 data FreeGen a where
@@ -23,4 +25,16 @@ instance Show Weight where show = show . toInt
 type Choice = Char
 
 -- Interpretations of FreeGen
+
+--- Random generator interpretation
+interpretAsG :: FreeGen a -> Gen a
+interpretAsG (Return v) = return v
+interpretAsG (Bind (Pick xs) f) = do
+  x <- Gen.frequency (map (\(Weight w,_,x) -> ((fromIntegral w), return x)) xs)
+  a <- interpretAsG x
+  interpretAsG (f a)
+-- The next statemant (Bind without Pick) will most likely not be used, but needs to be defined to check all definitions of Bind
+interpretAsG (Bind m f) = do
+  a <- interpretAsG m
+  interpretAsG (f a)
 
